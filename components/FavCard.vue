ca4c451ea82a37a49836733a8c6441f85cfe6598
Iranigami Sonorous <iranigami@mail.ru>
2024-12-09 14:08:08 +0400

    add temp sign

--- components/FavCard.vue
+++ components/FavCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-[158px] lg:w-[270px] h-[190px] rounded-[6px] bg-[#292E44] justify-center items-center pt-[23px]">
     <div class="text-very-white text-[14px] font-sfregular text-center leading-[15.5px]"> {{ city.name }}</div>
-    <div class="text-very-white text-[40px] font-sfmedium text-center mt-[4px]"> {{ Math.floor(city.weather.main.temp) }}</div>
+    <div class="text-very-white text-[40px] font-sfmedium text-center mt-[4px]"> {{ Math.floor(city.weather.main.temp) }}°</div>
     <img :src="city.icon" class="w-[100px] h-[100px] mx-auto -mt-[18px]"/>
   </div>
 </template>
